132ed4a086aa13e2db36530d5bc11cc9351b90c5
Soumya Mukherjee <mukherjee.soumya@gmail.com>
2019-11-13 18:28:12 +0530

    added properties file support & pageobject

--- src/test/java/coretests/mobileAppTest.java
+++ src/test/java/coretests/mobileAppTest.java
@@ -1,41 +1,47 @@
 package coretests;
 
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import pageobject.*;
 
 public class mobileAppTest {
 	
-	public static AndroidDriver<WebElement> driver;
-	public DesiredCapabilities capabilities;
+	static AndroidDriver<WebElement> driver;
+	DesiredCapabilities capabilities;
+	locator loc = new locator();
+	Properties prop;
+	InputStream stream;
 	
 	@BeforeSuite
 	public void setup() throws Exception{
 		
-		final String URL_STRING = "http://localhost:4444/wd/hub";
+		prop = new Properties();
+		String propname = "./config/application.properties";
+		stream = getClass().getClassLoader().getResourceAsStream(propname);
+		
+		prop.load(stream);
+		
+		final String URL_STRING = "http://" + prop.getProperty("servername") + ":" + prop.getProperty("port") +"/wd/hub";
 	    URL  url = new URL(URL_STRING);
 		
 	    capabilities = new DesiredCapabilities();
-	    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "oneplus7");
-	    //capabilities.setCapability(MobileCapabilityType.APP, "https://github.com/afollestad/material-dialogs/raw/master/sample/sample.apk");
+	    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, prop.getProperty("devicename"));
 	    capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
-	    capabilities.setCapability(MobileCapabilityType.UDID, "9799e5cb");
-	    capabilities.setCapability(MobileCapabilityType.APP, "/Users/soumyamukherjee/Documents/Working/Projects/CatseyeQA/mobileautomation/libs/apk/techkare_app.apk");
-	    capabilities.setCapability("appPackage", "com.catseye.techkare");
-	    capabilities.setCapability("appActivity", "com.catseye.techkare.MainActivity");
-	    capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+	    capabilities.setCapability(MobileCapabilityType.UDID, prop.getProperty("udid"));
+	    capabilities.setCapability(MobileCapabilityType.APP, prop.getProperty("apppath"));
+	    capabilities.setCapability("appPackage", prop.getProperty("apppackage"));
+	    capabilities.setCapability("appActivity", prop.getProperty("appactivity"));
 	    
 	    driver = new AndroidDriver<WebElement>(url, capabilities);
 	    driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
@@ -51,14 +57,14 @@ public class mobileAppTest {
 		//Thread.sleep should not be used - this is just a sample
 		Thread.sleep(20000);
 		System.out.println("Setting email");
-		driver.findElement(By.id("email")).click();
+		driver.findElement(By.id(loc.email_username)).click();
 		Thread.sleep(10000);
-		driver.getKeyboard().sendKeys("abc@abc.com");
+		driver.getKeyboard().sendKeys(prop.getProperty("appusername"));
 		Thread.sleep(10000);
-		driver.findElement(By.id("password")).click();
-		driver.getKeyboard().sendKeys("blahblahblah");
+		driver.findElement(By.id(loc.email_password)).click();
+		driver.getKeyboard().sendKeys(prop.getProperty("apppassword"));
 		Thread.sleep(10000);
-		driver.findElement(By.xpath("//*[@id=\"login\"]/div[3]/button")).click();
+		driver.findElement(By.xpath(loc.login_button)).click();
 		Thread.sleep(10000);
 	}
 	
---
+++ src/test/java/pageobject/locator.java
@@ -0,0 +1,10 @@
+package pageobject;
+
+public class locator {
+	
+	public String email_username = "email";
+	public String email_password = "password";
+	public String login_button = "//*[@id=\"login\"]/div[3]/button";
+	
+
+}
